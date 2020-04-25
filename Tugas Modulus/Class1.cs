4db155015377fb03890c9e2596751803137da634
silumankadal <bolehngasal@gmail.com>
2020-04-25 20:51:55 +0700

    first commit

---
+++ Tugas Modulus/Class1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tugas_Modulus
+{
+    class Class1
+    {
+        public string[,] data, histories; 
+        private string email, password, roles = "";
+        int book;
+
+        public Class1(string emails, string passwords)
+        {
+            email = emails;
+            password = passwords;
+            data = new string[2, 3] {
+                {"indriawanakbar@gmail.com", "12345", "superadmin" },
+                {"idris@gmail.com", "12345", "user"  }
+            };
+            histories = new String[2, 4] {
+                {"indriawanakbar@gmail.com", "Fisika Dasar", "Dasar Komputer dan Pemrograman", "25-04-2020" },
+                {"idris@gmail.com", "Dasar Komputer Pemrograman", "Konsep Jaringan Komputer","25-04-2020" }
+            };
+        }
+
+        public void login()
+        {
+            var role = checkCredentials();
+            if (role != "false")
+            {
+                Console.WriteLine("\nWelcome " + role);
+                Console.WriteLine("Logged it as user email: " + email);
+                Console.WriteLine(email + " meminjam buku: ");
+                for (int i=1; i<3; i++)
+                {
+                    Console.WriteLine(histories[book, i]);
+                }
+                Console.WriteLine("Tanggal pinjaman : " + histories[book, 3]);
+            }
+            else
+            {
+                Console.WriteLine("\nInvalid Login");
+            }
+        }
+
+        private string checkCredentials()
+        {
+            for (int i = 0; i < data.GetLength(0); i++)
+            {
+                if (data[i, 0] == email && data[i, 1] == password)
+                {
+                    if (data[i, 0] == histories[i, 0])
+                        book = i;
+                    roles = data[i, 2];
+                    return roles;
+                }
+            }
+            return "false";
+        }
+    }
+}
---
+++ Tugas Modulus/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tugas_Modulus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string email, password;
+
+            Console.Write("Email: ");
+            email = Console.ReadLine();
+            Console.Write("Password: ");
+            password = Console.ReadLine();
+
+            Class1 data = new Class1(email, password);
+            data.login();
+
+            Console.ReadKey();
+        }
+    }
+}
